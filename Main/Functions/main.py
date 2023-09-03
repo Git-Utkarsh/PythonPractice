cae3ddd2313ce54417bf11ecc92f206a07d4ac3f
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-03 10:11:23 +0530

    Update main.py

--- Main/Functions/main.py
+++ Main/Functions/main.py
@@ -1,1 +1,8 @@
-m
+#Question: Write a Python program that calculates the factorial of a given positive integer. The factorial of a number "n" is
+#the product of all positive integers from 1 to "n." For example, the factorial of 5 (written as 5!) is 5 x 4 x 3 x 2 x 1 = 120.
+
+# Answer
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1 
+    return n * factorial(n-1)
