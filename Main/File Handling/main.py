ac578fb99093aa66849d20a49cfd3a7c4ff3ec12
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-03 10:06:23 +0530

    Update main.py

--- Main/File Handling/main.py
+++ Main/File Handling/main.py
@@ -1,1 +1,12 @@
+## Question: Write a Python program that reads the contents of a text file named "example.txt" (assuming the file exists) and prints each line with line numbers. For example:
+#1: This is the first line.
+#2: This is the second line.
+#3: This is the third line.
 
+#Answer
+with open("example.txt") as file:
+    line = file.readlines()
+    count = 1
+    for i in line:
+        print(str(count) + ": " + i)
+        count=count+1
