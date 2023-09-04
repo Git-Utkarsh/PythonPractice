bcde35d55366dbb3dde9d55d8b5ef83906cd6189
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-04 23:23:32 +0530

    Update main.py

--- Main/Lists/main.py
+++ Main/Lists/main.py
@@ -1,1 +1,8 @@
+#Question: Write a Python function that takes a list of numbers as input and returns the sum of all the even numbers in the list.
 
+def even_add(lst):
+    l = []
+    for i in lst:
+        if i%2==0:
+            l.append(i)
+    return sum(l)
