e2b9f2c4c7ff52ab80734be9096bf082fd4a34bc
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-05 18:49:25 +0530

    Update main.py

--- Main/Strings/main.py
+++ Main/Strings/main.py
@@ -1,1 +1,4 @@
+#Question: Write a Python function that takes a string as input and returns the string with all the vowels replaced by the character '#'. For example, if the input is "Hello, how are you?", the function should return "H#ll#, h#w #r# yo#?".
 
+def vowel_replace(strin):
+    return strin.replace("a","#").replace("e","#").replace("i","#").replace("o","#").replace("u","#")
