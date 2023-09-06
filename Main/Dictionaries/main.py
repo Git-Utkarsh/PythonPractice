d5532f3c32f6d907d18446adc8f3314326b1894b
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-06 11:22:01 +0530

    Update main.py

--- Main/Dictionaries/main.py
+++ Main/Dictionaries/main.py
@@ -1,1 +1,8 @@
+#Question: Write a Python function that takes a dictionary as input, where the keys are student names, and the values are their corresponding scores. The function should return the name of the student with the highest score.
 
+def max_marks(dictionary):
+    for i in dictionary:
+        lst = list(dictionary.values())
+        if dictionary[i]==max(lst):
+            return i 
+        
