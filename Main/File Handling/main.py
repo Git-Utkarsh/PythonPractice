0d4d4acf234f7c8a2d248391f64ffbecc4b301fd
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-07 10:23:44 +0530

    Update main.py

--- Main/File Handling/main.py
+++ Main/File Handling/main.py
@@ -10,3 +10,16 @@ with open("example.txt") as file:
     for i in line:
         print(str(count) + ": " + i)
         count=count+1
+
+#Question: Write a Python program that reads the contents of a text file named "data.txt" 
+#(assuming the file exists) and counts the frequency of each word in the file. Then, print out each unique word and its corresponding frequency.
+
+#Answer
+with open("data.txt","r") as f:
+    a = f.readlines()
+    lst = []
+    for i in a:
+        for k in i.split():
+            lst.append(k.lower())
+    for j in set(lst):
+        print(j +" Occur " +  str(lst.count(j)) + " Times")
