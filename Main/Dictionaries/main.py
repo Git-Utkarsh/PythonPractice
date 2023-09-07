d0e0ee56ae1cda0c232939272322867b6f44ed1c
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-07 20:52:04 +0530

    Update main.py

--- Main/Dictionaries/main.py
+++ Main/Dictionaries/main.py
@@ -6,3 +6,13 @@ def max_marks(dictionary):
         if dictionary[i]==max(lst):
             return i 
         
+#Question: Write a Python function that takes a list of dictionaries as input, where each dictionary represents a student and their scores in different subjects. The dictionaries have the following 
+#format: {"Name": "StudentName", "Math": Score, "Science": Score, "History": Score}. 
+#The function should calculate the average score for each student and return a new list of dictionaries, where each dictionary includes the student's name and their average score.
+
+def average(lst):
+    lst1 = []
+    for item in lst:
+        dict = {item["Name"]:str((item["Math"] + item["Science"] + item["History"])/3)}
+        lst1.append(dict)
+    return lst1
