0d4efc3dc205083a325fd4622609d4c0fe8a5c5f
Utkarsh <84007458+Git-Utkarsh@users.noreply.github.com>
2023-09-07 10:47:16 +0530

    Update main.py

--- Main/File Handling/main.py
+++ Main/File Handling/main.py
@@ -34,3 +34,26 @@ with open("input.txt","r") as f:
     a.append("\nFile copied successfully.")
 with open("output.txt","w") as n:
     n.writelines(a)
+
+
+#Question: Write a Python program that reads a CSV file named "data.csv" (assuming the file exists). Each line of the CSV file contains 
+#information about a student in the following format: Name,Score. Your task is to calculate and print the average score of all the students.
+
+#Answer
+import csv
+import statistics
+with open("data.csv","r",newline='\r\n') as f:
+    read = csv.reader(f)
+    count = 0
+    lst = []
+    for rec in read:
+        for item in rec:
+            if count%2==0:
+                pass
+            else:
+                lst.append(item)
+            count += 1
+lst2=[]
+for i in lst:
+    lst2.append(int(i))
+print(statistics.mean(lst2))
